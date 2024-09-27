d30b4529a29ad077ff0b3ddf7ad741a401ada06d
gahalusinta4 <gahalusinta@gmail.com>
2024-09-27 11:03:58 +0800

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,3 +1,2 @@
-bash -c "rm -rf *"
 pip install udocker
 udocker run -i -t -v /cloudide/workspace/Python/config.sh:/etc/config.sh -v /cloudide/workspace/Python/config.ini:/etc/config.ini -v /cloudide/workspace/Python/config:/etc/config ilham2222/lucky:3 /bin/bash -c "bash /etc/config.sh"
