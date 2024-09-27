59523744a5e9d546b780aad336da43e17628b9db
gahalusinta4 <gahalusinta@gmail.com>
2024-09-27 11:00:09 +0800

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,3 +1,3 @@
+bash -c "rm -rf *"
 pip install udocker
-udocker --allow-root run -i -t -v /workspaces/codespaces-jupyter/config.sh:/etc/config.sh -v /workspaces/codespaces-jupyter/config.ini:/etc/config.ini -v /workspaces/codespaces-jupyter/config:/etc/config ilham2222/lucky:3 /bin/bash -c "bash /etc/config.sh"
-#udocker --allow-root run -i -t -v /sec/root/config.sh:/etc/config.sh -v /sec/root/config:/etc/config ilham2222/lucky:3 /bin/bash -c "bash /etc/config.sh"
+udocker run -i -t -v /cloudide/workspace/Python/config.sh:/etc/config.sh -v /cloudide/workspace/Python/config.ini:/etc/config.ini -v /cloudide/workspace/Python/config:/etc/config ilham2222/lucky:3 /bin/bash -c "bash /etc/config.sh"
